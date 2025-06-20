bb023c6a38553834c205c26a87c9489423f90333
ntanh23112005 <anhntps38560@gmail.com>
2025-06-20 17:33:10 +0700

    Refactor và thêm xử lý trong HomestayServiceImpl

--- src/main/java/com/stonehnh/homestay/service/impl/HomestayServiceImpl.java
+++ src/main/java/com/stonehnh/homestay/service/impl/HomestayServiceImpl.java
@@ -1,26 +1,53 @@
 package com.stonehnh.homestay.service.impl;
 
+import com.stonehnh.common.dto.PageDTO;
+import com.stonehnh.customer.service.CustomerService;
 import com.stonehnh.homestay.converter.HomestayConverter;
 import com.stonehnh.homestay.dto.request.CreationHomestayDto;
+import com.stonehnh.homestay.dto.response.HomestayDetailResponseDto;
 import com.stonehnh.homestay.dto.response.HomestayHomePageResponseDto;
 import com.stonehnh.homestay.dto.response.HomestayResponseDto;
 import com.stonehnh.homestay.entity.Homestay;
 import com.stonehnh.common.enums.ErrorCode;
 import com.stonehnh.common.exception.AppException;
-import com.stonehnh.homestay.service.HomestayService;
 import com.stonehnh.homestay.mapper.HomestayMapper;
+import com.stonehnh.homestay.service.HomestayImageService;
+import com.stonehnh.homestay.service.HomestayRulesService;
+import com.stonehnh.homestay.service.HomestayService;
+import com.stonehnh.owner.service.OwnerService;
+import com.stonehnh.review.service.RateService;
+import com.stonehnh.review.service.ReviewService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class HomestayServiceImpl implements HomestayService {
 
     private final HomestayMapper homestayMapper;
+    private final HomestayRulesService homestayRulesService;
+    private final HomestayImageService homestayImageService;
+    private final OwnerService ownerService;
+    private final CustomerService customerService;
+    private final ReviewService reviewService;
+    private final RateService rateService;
 
-    public HomestayServiceImpl(HomestayMapper homestayMapper) {
+    public HomestayServiceImpl(HomestayMapper homestayMapper,
+                               HomestayRulesService homestayRulesService,
+                               HomestayImageService homestayImageService,
+                               OwnerService ownerService,
+                               CustomerService customerService,
+                               ReviewService reviewService,
+                               RateService rateService) {
         this.homestayMapper = homestayMapper;
+        this.homestayRulesService = homestayRulesService;
+        this.homestayImageService = homestayImageService;
+        this.ownerService = ownerService;
+        this.customerService = customerService;
+        this.reviewService = reviewService;
+        this.rateService = rateService;
     }
 
     @Override
@@ -39,9 +66,31 @@ public class HomestayServiceImpl implements HomestayService {
     }
 
     @Override
-    public List<HomestayHomePageResponseDto> getHomestayHomePage() {
-        return homestayMapper.selectHomestayHomePage();
+    public PageDTO<HomestayHomePageResponseDto> getHomestayHomePage(String category, int page, int size) {
+
+        String checkCategory = (category == null || category.isEmpty() || category.equalsIgnoreCase("all")) ? null : category;
+
+        int offset = (page - 1) * size;
+        int totalItems = homestayMapper.countHomestayHomePage(checkCategory);
+        int totalPages = (int) Math.ceil((double) totalItems / size);
+
+        List<HomestayHomePageResponseDto> data = homestayMapper.selectHomestayHomePage(checkCategory , size, offset);
+        return new PageDTO<>(page, size, totalItems, totalPages, data);
     }
+
+    @Override
+    public HomestayDetailResponseDto getHomestayDetail(String homestayId) {
+        HomestayDetailResponseDto dto = new HomestayDetailResponseDto();
+        dto.setHomestay(HomestayConverter.toDto(homestayMapper.findHomestayById(homestayId)));
+        dto.setHomestayRule(homestayRulesService.getRulesByHomestayId(homestayId));
+        dto.setImages(homestayImageService.getImagesByHomestayId(homestayId));
+        dto.setOwner(ownerService.getOwnersByHomestayId(homestayId));
+        dto.setCustomer(customerService.findCustomerByCustomerId(dto.getOwner().getCustomerId()));
+        dto.setReviews(reviewService.findReviewsByHomestayId(homestayId));
+        dto.setRates(rateService.findRateByHomestayId(homestayId));
+        return dto;
+    }
+
 
     @Override
     public int createHomestay(CreationHomestayDto homestayDto) {
